c355a6c689e93ef23638a34aab67b708413b73de
Illia Shkroba <is@pjwstk.edu.pl>
2025-02-21 00:03:22 +0100

    Change colors

--- xmobar.hs
+++ xmobar.hs
@@ -5,7 +5,7 @@ config =
   defaultConfig
     { alignSep = "}{"
     , allDesktops = True
-    , bgColor = "#5f5f5f"
+    , bgColor = "#1e1e2e"
     , commands =
         [ Run $
           Cpu ["-L", "3", "-H", "50", "--high", "red", "--normal", "green"] 10
@@ -21,7 +21,7 @@ config =
         , Run $ Date "%a %Y-%m-%d <fc=#8be9fd>%H:%M</fc>" "date" 10
         , Run XMonadLog
         ]
-    , fgColor = "#f8f8f2"
+    , fgColor = "#cdd6f4"
     , font = "xft:Noto Sans:pixelsize=13,M+ 1c:pixelsize=13"
     , overrideRedirect = False
     , position = TopW L 100
